1c69eedc3f607dd99a55f7fef57f443b95bb8c60
steve <steve@util.localdomain>
2017-09-23 15:31:07 -0400

    now the header won't run if the data file exists

--- stroop_header_gen.rb
+++ stroop_header_gen.rb
@@ -1,17 +1,21 @@
 #!/usr/bin/env ruby
 
-headr = ''
-subj = "subject"
-sess = "session"
-
-headr += "#{subj},#{sess},"
-
-192.times do 
-  ['rtt','acc','cycle'].each do |type|
-	  headr += "#{type.strip},"
+#don't do this if the file exists
+unless File.file?('stroop_ruby_out.csv') 
+  headr = ''
+  subj = "subject"
+  sess = "session"
+  
+  headr += "#{subj},#{sess},"
+  
+  192.times do 
+    ['rtt','acc','cycle'].each do |type|
+  	  headr += "#{type.strip},"
+    end
   end
+  
+  headr += "\n"
+  
+  File.open('stroop_ruby_out.csv','a') { |f| f.write( headr ) }
 end
 
-headr += "\n"
-
-File.open('stroop_ruby_out.csv','a') { |f| f.write( headr ) }
