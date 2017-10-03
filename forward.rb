5b8614ccdf6907faf4bec93338782f3857197f2e
Steve Newsted <newsted@gmail.com>
2017-10-02 21:55:13 -0400

    new forward script logic

--- forward.rb
+++ forward.rb
@@ -2,120 +2,51 @@
 
 #print ARGV[0]
 
-# loop across this to append to csv to get all the forward files, but for now, one file
-if ARGV[0].nil?
-  puts 'tell me a file to use, idiot'
-else
-  workingfile = ARGV[0]
-  
-  # only if this is a real file, do the needful
-  if File.file?(workingfile) 
-  
-    # Search the files for 'subject or session' then grab the first result, then split that result down the last word (the number)
-    subject = File.open(workingfile).grep(/Subject:/).first.split.last
-    session = File.open(workingfile).grep(/Session:/).first.split.last
-    
-    # set the subject/session info at the beginning of the row
-    datarecord = "#{subject},#{session},"
-  
-    # how many practice quesitons i believe there are 
-    practices = 2
+# loop across this to append to csv to get all the stroop files, but for now, one file
+workingfile = ARGV[0]
 
-    # loop through file and make an array for all of the things we want
-    responses = Hash.new
-    responses['Procedure'] 	= []
-    responses['First'] 		= []
-    responses['Second'] 	= []
-    responses['Third'] 		= []
-    responses['Fourth'] 	= []
-    responses['Fifth'] 		= []
-    responses['Sixth'] 		= []
-    responses['Seventh'] 	= []
-    responses['Eight'] 		= []
-    responses['KeyPress1RT'] 	= []
-    responses['KeyPress1RESP'] 	= []
-    responses['KeyPress2RT'] 	= []
-    responses['KeyPress2RESP'] 	= []
-    responses['KeyPress3RT'] 	= []
-    responses['KeyPress3RESP'] 	= []
-    responses['KeyPress4RT'] 	= []
-    responses['KeyPress4RESP'] 	= []
-    responses['KeyPress5RT'] 	= []
-    responses['KeyPress5RESP'] 	= []
-    responses['KeyPress6RT'] 	= []
-    responses['KeyPress6RESP'] 	= []
-    responses['KeyPress7RT'] 	= []
-    responses['KeyPress7RESP'] 	= []
-    responses['KeyPress8RT'] 	= []
-    responses['KeyPress8RESP'] 	= []
+trial = 0
 
-    # search through the file for any possible values we may want and create an array of all of it
-    #bigpull = File.open(workingfile).grep(/Trials.Sample:|Score:|Image.RT:/)
-    
-    # loop to clean whitespace from ALL the strings in the array (even strings we don't want)
-    #bigpull.each do  |x| 
-    #	x = x.strip!  # the ! permanently writes back to the array instead of the temporary values
-    #end 
-    #
-    #bigpull.each do |x| 
-    ## skip the entries that say "TrialProc" etc, because they don't actually contain data
-    #  unless x =~ /Trials/ 
-    #    # append data to the end of this record, separated by commas
-    #    datarecord += "#{x.split.last},"
-    #  end
-    #end 
+# only if this is a real file, do the needful
+if File.file?(workingfile) 
+
+  # Search the files for 'subject or session' then grab the first result, then split that result down the last word (the number)
+  subject = File.open(workingfile).grep(/Subject:/).first.split.last
+  session = File.open(workingfile).grep(/Session:/).first.split.last
   
-    responses.each do |item|
-      type = item.first
-      print "#{type}\n"
-      responses[type] = File.open(workingfile).grep(/#{type}/)
-      responses[type].each do |x|
-        x = x.split.last if practices > 0 
-        print "#{x},"
-      end
-      print "\n"
-      practices = practices - 1
+  # set the subject/session info at the beginning of the row
+  datarecord = "#{subject},#{session},"
+
+  # search through the file for any possible values we may want and create an array of all of it
+  bigpull = File.open(workingfile).grep(/Procedure|DigitSpan|First|Second|Third|Fourth|Fifth|Sixth|Seventh|Eigth|KeyPress1RT|KeyPress|KeyPress1RT|KeyPress1RESP|KeyPress2RT|KeyPress2RESP|KeyPress3RT|KeyPress3RESP|KeyPress4RT|KeyPress4RESP|KeyPress5RT|KeyPress5RESP|KeyPress6RT|KeyPress6RESP|KeyPress7RT|KeyPress7RESP|KeyPress8RT|KeyPress8RESP/)
+  
+  # loop to clean whitespace from ALL the strings in the array (even strings we don't want)
+  bigpull.each do  |x| 
+  	x = x.strip!  # the ! permanently writes back to the array instead of the temporary values
+  end 
+  
+  # grab the first instance of the TrialProcMain line, which is the start of the REAL data
+  first_idx = bigpull.index('Procedure: Length4')
+  puts bigpull
+  
+  # remove the first chunk of the array before the pieces of info that matter -- this drops all the test data
+  bigpull.shift(first_idx)
+  
+  bigpull.each do |x| 
+  # skip the entries that say "Procedure" etc, because they don't actually contain data
+    if x =~ /Procedure/ 
+      datarecord += "\n" unless trial == 0
+      trial = trial + 1
+      datarecord += "#{subject},"
+      datarecord += "#{session},"
+      datarecord += "#{trial},"
+    else
+      # if it's the sample number we insert a newline to start a new row
+      datarecord += "#{x.split.last},"
     end
-  
-   # there are 15 questions, so we need to loop over the data that many times, 
-   # and include blanks when there are no datapoints
- #  15.times do |x|
- #    responses.each do |type|
- #          headr += "#{type.strip} #{x+1},"
- #    end
- #  end
- #
-    #puts responses['First']
-    #puts responses['First'] 
-    #puts responses['Second']
-    #puts responses['Third'] 
-    #puts responses['Fourth']
-    #puts responses['Fifth'] 
-    #puts responses['Sixth']
-    #puts responses['Seventh'] 	
-    #puts responses['Eight'] 
-    #puts responses['KeyPress1RT']
-    #puts responses['KeyPress1RESP']
-    #puts responses['KeyPress2RT'] 	
-    #puts responses['KeyPress2RESP'] 
-    #puts responses['KeyPress3RT']
-    #puts responses['KeyPress3RESP']
-    #puts responses['KeyPress4RT'] 
-    #puts responses['KeyPress4RESP']
-    #puts responses['KeyPress5RT'] 
-    #puts responses['KeyPress5RESP']
-    #puts responses['KeyPress6RT'] 
-    #puts responses['KeyPress6RESP']
-    #puts responses['KeyPress7RT'] 
-    #puts responses['KeyPress7RESP']
-    #puts responses['KeyPress8RT'] 
-    #puts responses['KeyPress8RESP']
+  end 
 
-    # add a newline at the end of this record, so the next won't have to know to do it
-    #datarecord += "\n"
-    
-    #puts datarecord
-    #File.open('forward_ruby_out.csv', 'a') {|f| f.write(datarecord) }
-  
-  end
+  #puts datarecord
+  File.open('stroop_ruby_out.csv', 'a') {|f| f.write(datarecord) }
+
 end
