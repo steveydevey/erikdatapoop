608a6a28839c0a72076e588b853a63ab4c3f3eb1
Steve Newsted <newsted@gmail.com>
2017-10-02 21:54:06 -0400

    new header gen, and sample csv i guess

--- forward_header_gen.rb
+++ forward_header_gen.rb
@@ -1,21 +1,10 @@
 #!/usr/bin/env ruby
 
 #don't do this if the file exists
-unless File.file?('nback_ruby_out.csv') 
-  headr = ''
-  subj = "subject"
-  sess = "session"
+unless File.file?('forward_ruby_out.csv') 
   
-  headr += "#{subj},#{sess},"
-  
-  60.times do |x|
-    ['score','imagert'].each do |type|
-  	  headr += "#{type.strip} #{x+1},"
-    end
-  end
-  
-  headr += "\n"
-  
-  File.open('nback_ruby_out.csv','a') { |f| f.write( headr ) }
+  header = "Session:,Subject:,Trials.Sample:,DigitSpan:,First:,Second:,Third:,Fourth:,Fifth:,Sixth:,Seventh:,Eighth:,KeyPress1RT:,KeyPress1RESP:,KeyPress2RT:,KeyPress2RESP:,KeyPress3RT:,KeyPress3RESP:,KeyPress4RT:,KeyPress4RESP:,KeyPress5RT:,KeyPress5RESP:,KeyPress6RT:,KeyPress6RESP:,KeyPress7RT: ,KeyPress7RESP: ,KeyPress8RT:,KeyPress8RESP:"
+
+  File.open('forward_ruby_out.csv','a') { |f| f.write( header ) }
 end
 
