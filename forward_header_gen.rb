ff14dd001c6ac929695597d1e44a1accfe4bd927
Steve Newsted <newsted@gmail.com>
2017-10-02 21:56:25 -0400

    removed spaces from forward gen

--- forward_header_gen.rb
+++ forward_header_gen.rb
@@ -3,7 +3,7 @@
 #don't do this if the file exists
 unless File.file?('forward_ruby_out.csv') 
   
-  header = "Session:,Subject:,Trials.Sample:,DigitSpan:,First:,Second:,Third:,Fourth:,Fifth:,Sixth:,Seventh:,Eighth:,KeyPress1RT:,KeyPress1RESP:,KeyPress2RT:,KeyPress2RESP:,KeyPress3RT:,KeyPress3RESP:,KeyPress4RT:,KeyPress4RESP:,KeyPress5RT:,KeyPress5RESP:,KeyPress6RT:,KeyPress6RESP:,KeyPress7RT: ,KeyPress7RESP: ,KeyPress8RT:,KeyPress8RESP:"
+  header = "Session:,Subject:,Trials.Sample:,DigitSpan:,First:,Second:,Third:,Fourth:,Fifth:,Sixth:,Seventh:,Eighth:,KeyPress1RT:,KeyPress1RESP:,KeyPress2RT:,KeyPress2RESP:,KeyPress3RT:,KeyPress3RESP:,KeyPress4RT:,KeyPress4RESP:,KeyPress5RT:,KeyPress5RESP:,KeyPress6RT:,KeyPress6RESP:,KeyPress7RT:,KeyPress7RESP:,KeyPress8RT:,KeyPress8RESP:"
 
   File.open('forward_ruby_out.csv','a') { |f| f.write( header ) }
 end
